46262ca3f327e99a7b01bf631873594a6636d59b
GitHub Action <action@github.com>
2022-10-10 02:16:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/thilankabowala/securecodingguideline/MyBundle.kt
+++ src/main/kotlin/com/github/thilankabowala/securecodingguideline/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.thilankabowala.securecodingguideline
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/thilankabowala/securecodingguideline/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/thilankabowala/securecodingguideline/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.thilankabowala.securecodingguideline.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.thilankabowala.securecodingguideline.MyBundle
 
 class MyApplicationService {
 
