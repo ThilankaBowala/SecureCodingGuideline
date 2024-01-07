9c2f347298ee9c18585fbdb0a3cb9dd7ee352d29
Thilanka Bowala <thilankabowala@gmail.com>
2024-01-07 11:44:04 +0530

    Plugin display content

--- src/main/java/Tools/MyToolWindowFactory.java
+++ src/main/java/Tools/MyToolWindowFactory.java
@@ -1,13 +1,17 @@
 package Tools;
+import com.intellij.execution.filters.TextConsoleBuilderFactory;
+import com.intellij.execution.ui.ConsoleView;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.*;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
 
 public class MyToolWindowFactory implements ToolWindowFactory {
     @Override
     public void createToolWindowContent(Project project, ToolWindow toolWindow) {
-//        ConsoleView consoleView = TextConsoleBuilderFactory.getInstance().createBuilder(project).getConsole();
-//        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
-//        Content content = contentFactory.createContent(consoleView.getComponent(), "Results", false);
-//        toolWindow.getContentManager().addContent(content);
+        ConsoleView consoleView = TextConsoleBuilderFactory.getInstance().createBuilder(project).getConsole();
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(consoleView.getComponent(), "Results", false);
+        toolWindow.getContentManager().addContent(content);
     }
 }
