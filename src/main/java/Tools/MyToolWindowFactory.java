4a436584c78dd662d9752e29bfa5c496696f2f21
Thilanka Bowala <thilankabowala@gmail.com>
2024-01-08 20:52:32 +0530

    Apply code suggestions

--- src/main/java/Tools/MyToolWindowFactory.java
+++ src/main/java/Tools/MyToolWindowFactory.java
@@ -5,10 +5,11 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.*;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
 
 public class MyToolWindowFactory implements ToolWindowFactory {
     @Override
-    public void createToolWindowContent(Project project, ToolWindow toolWindow) {
+    public void createToolWindowContent(@NotNull Project project, ToolWindow toolWindow) {
         ConsoleView consoleView = TextConsoleBuilderFactory.getInstance().createBuilder(project).getConsole();
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content content = contentFactory.createContent(consoleView.getComponent(), "Results", false);
