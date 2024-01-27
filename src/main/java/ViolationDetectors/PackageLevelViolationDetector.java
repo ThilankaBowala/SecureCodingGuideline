53f2a2debdcbf8be20b49255fbecb36bcc2025e3
Thilanka Bowala <thilankabowala@gmail.com>
2024-01-27 13:01:16 +0530

    Code refactor

--- src/main/java/ViolationDetectors/PackageLevelViolationDetector.java
+++ src/main/java/ViolationDetectors/PackageLevelViolationDetector.java
@@ -9,7 +9,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import java.util.ArrayList;
 import java.util.Map;
 
-public class PackageLevelViolationDetector extends AnAction implements  ViolationDetector  {
+public class PackageLevelViolationDetector extends AnAction implements ViolationDetector {
     String valueTHI00J = "";
     String valueSER01J = "";
     String valueNUM10J = "";
@@ -25,7 +25,7 @@ public class PackageLevelViolationDetector extends AnAction implements  Violatio
     @Override
     public String rule1Detection() {
         try {
-            rule1Detection=detectViolationTHI00J();
+            rule1Detection = detectViolationTHI00J();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -35,7 +35,7 @@ public class PackageLevelViolationDetector extends AnAction implements  Violatio
     @Override
     public String rule2Detection() {
         try {
-            rule2Detection=detectViolationSER01J();
+            rule2Detection = detectViolationSER01J();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -44,7 +44,7 @@ public class PackageLevelViolationDetector extends AnAction implements  Violatio
 
     public String rule3Detection() {
         try {
-            rule3Detection=detectViolationNUM10J();
+            rule3Detection = detectViolationNUM10J();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -53,7 +53,7 @@ public class PackageLevelViolationDetector extends AnAction implements  Violatio
 
     public String rule4Detection() {
         try {
-            rule4Detection=detectViolationSEC07J();
+            rule4Detection = detectViolationSEC07J();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -62,7 +62,7 @@ public class PackageLevelViolationDetector extends AnAction implements  Violatio
 
     public String rule5Detection() {
         try {
-            rule5Detection=detectViolationFIO02J();
+            rule5Detection = detectViolationFIO02J();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -73,8 +73,7 @@ public class PackageLevelViolationDetector extends AnAction implements  Violatio
     public void actionPerformed(AnActionEvent e) {
     }
 
-    public String detectViolationTHI00J() throws Exception
-    {
+    public String detectViolationTHI00J() throws Exception {
         PackageLevelCodeFragment cc1 = new PackageLevelCodeFragment();
         MethodLevelCodeFragment cc2 = new MethodLevelCodeFragment();
         boolean flag_interface = false;
@@ -82,8 +81,7 @@ public class PackageLevelViolationDetector extends AnAction implements  Violatio
             ArrayList<Integer> lines = new ArrayList<Integer>();
             ArrayList<Integer> columns = new ArrayList<Integer>();
             ArrayList<Integer> ends = new ArrayList<Integer>();
-            for (int j=0; j<cc1.ImplementedInterfaces.size(); j++)
-            {
+            for (int j = 0; j < cc1.ImplementedInterfaces.size(); j++) {
                 if (cc1.ImplementedInterfaces.get(j).equals("Runnable")) {
                     flag_interface = true;
                 }
@@ -105,8 +103,7 @@ public class PackageLevelViolationDetector extends AnAction implements  Violatio
         return valueTHI00J;
     }
 
-    public String detectViolationSER01J() throws Exception
-    {
+    public String detectViolationSER01J() throws Exception {
         PackageLevelCodeFragment cc1 = new PackageLevelCodeFragment();
         ClassLevelCodeFragment cc2 = new ClassLevelCodeFragment();
         boolean flag_serializable = false;
@@ -115,8 +112,7 @@ public class PackageLevelViolationDetector extends AnAction implements  Violatio
         ArrayList<Integer> columns = new ArrayList<Integer>();
         ArrayList<Integer> ends = new ArrayList<Integer>();
 
-        for(Map.Entry<String, ArrayList<Integer>> entry : cc2.methodSignatures.entrySet())
-        {
+        for (Map.Entry<String, ArrayList<Integer>> entry : cc2.methodSignatures.entrySet()) {
             violation = true;
             lines.add(entry.getValue().get(0));
             columns.add(entry.getValue().get(1));
@@ -124,15 +120,12 @@ public class PackageLevelViolationDetector extends AnAction implements  Violatio
 
 
         }
-        for(int j=0; j< cc1.ImplementedInterfaces.size(); j++)
-        {
-            if(cc1.ImplementedInterfaces.get(j).equals("Serializable"))
-            {
+        for (int j = 0; j < cc1.ImplementedInterfaces.size(); j++) {
+            if (cc1.ImplementedInterfaces.get(j).equals("Serializable")) {
                 flag_serializable = true;
             }
         }
-        if(violation && flag_serializable)
-        {
+        if (violation && flag_serializable) {
             lce.put("package_rule2_line", lines);
             lce.put("package_rule2_column", columns);
             lce.put("package_rule2_end", ends);
@@ -142,17 +135,16 @@ public class PackageLevelViolationDetector extends AnAction implements  Violatio
         valueSER01J = "";
         return valueSER01J;
     }
-    public String detectViolationNUM10J() throws Exception
-    {
+
+    public String detectViolationNUM10J() throws Exception {
         ArrayList<Integer> lines = new ArrayList<Integer>();
         ArrayList<Integer> columns = new ArrayList<Integer>();
         ArrayList<Integer> ends = new ArrayList<Integer>();
         boolean violation = false;
         MethodLevelCodeFragment cc1 = new MethodLevelCodeFragment();
-        if(cc1.ObjectCReationExpress.size()>0){
-            for(int i=0;i<cc1.ObjectCReationExpress.size(); i++)
-            {
-                if (((cc1.ObjectCReationExpress.get(i).getArguments().size()>0)&&(cc1.ObjectCReationExpress.get(i).getArguments().get(0).isDoubleLiteralExpr()))&&(cc1.ObjectCReationExpress.get(i).getTypeAsString().equals("BigDecimal"))){
+        if (cc1.ObjectCReationExpress.size() > 0) {
+            for (int i = 0; i < cc1.ObjectCReationExpress.size(); i++) {
+                if (((cc1.ObjectCReationExpress.get(i).getArguments().size() > 0) && (cc1.ObjectCReationExpress.get(i).getArguments().get(0).isDoubleLiteralExpr())) && (cc1.ObjectCReationExpress.get(i).getTypeAsString().equals("BigDecimal"))) {
                     lines.add(cc1.ObjectCReationExpress.get(i).getArguments().get(0).getBegin().get().line);
                     columns.add(cc1.ObjectCReationExpress.get(i).getArguments().get(0).getBegin().get().column);
                     ends.add(cc1.ObjectCReationExpress.get(i).getArguments().get(0).getEnd().get().column);
@@ -161,8 +153,7 @@ public class PackageLevelViolationDetector extends AnAction implements  Violatio
                 }
             }
         }
-        if(violation)
-        {
+        if (violation) {
             lce.put("package_rule3_line", lines);
             lce.put("package_rule3_column", columns);
             lce.put("package_rule3_end", ends);
@@ -172,8 +163,7 @@ public class PackageLevelViolationDetector extends AnAction implements  Violatio
         return valueNUM10J;
     }
 
-    public String detectViolationSEC07J() throws Exception
-    {
+    public String detectViolationSEC07J() throws Exception {
         MethodLevelCodeFragment cc1 = new MethodLevelCodeFragment();
         ClassLevelCodeFragment cc2 = new ClassLevelCodeFragment();
         boolean violation = false;
@@ -182,18 +172,14 @@ public class PackageLevelViolationDetector extends AnAction implements  Violatio
         ArrayList<Integer> ends = new ArrayList<Integer>();
         int lineNo;
 
-        if((!cc2.methodDeclarations.isEmpty())&&(!cc1.ObjectCReationExpress.isEmpty())){
-            for(int i = 0; i<cc2.methodDeclarations.size(); i++)
-            {
-                if(cc2.methodDeclarations.get(i).getNameAsString().equals("getPermissions"))
-                {
-                    int getPermissions_start=cc2.methodDeclarations.get(i).getBody().get().getRange().get().begin.line;
-                    int getPermissions_end=cc2.methodDeclarations.get(i).getBody().get().getRange().get().end.line;
-                    for(int j=0;j<cc1.ObjectCReationExpress.size();j++)
-                    {
+        if ((!cc2.methodDeclarations.isEmpty()) && (!cc1.ObjectCReationExpress.isEmpty())) {
+            for (int i = 0; i < cc2.methodDeclarations.size(); i++) {
+                if (cc2.methodDeclarations.get(i).getNameAsString().equals("getPermissions")) {
+                    int getPermissions_start = cc2.methodDeclarations.get(i).getBody().get().getRange().get().begin.line;
+                    int getPermissions_end = cc2.methodDeclarations.get(i).getBody().get().getRange().get().end.line;
+                    for (int j = 0; j < cc1.ObjectCReationExpress.size(); j++) {
                         lineNo = cc1.ObjectCReationExpress.get(j).getBegin().get().line;
-                        if((cc1.ObjectCReationExpress.get(j).getTypeAsString().equals("Permissions"))&&((getPermissions_start<=lineNo)&&(lineNo<=getPermissions_end)))
-                        {
+                        if ((cc1.ObjectCReationExpress.get(j).getTypeAsString().equals("Permissions")) && ((getPermissions_start <= lineNo) && (lineNo <= getPermissions_end))) {
                             lines.add(cc1.ObjectCReationExpress.get(j).getBegin().get().line);
                             columns.add(cc1.ObjectCReationExpress.get(j).getBegin().get().column);
                             ends.add(cc1.ObjectCReationExpress.get(j).getEnd().get().column);
@@ -203,36 +189,31 @@ public class PackageLevelViolationDetector extends AnAction implements  Violatio
                 }
             }
         }
-        if(violation){
+        if (violation) {
             lce.put("package_rule4_line", lines);
             lce.put("package_rule4_column", columns);
             lce.put("package_rule4_end", ends);
             valueSEC07J = "SEC07J violated at " + lines;
             return valueSEC07J;
         }
-        return  valueSEC07J;
+        return valueSEC07J;
     }
 
-    public String detectViolationFIO02J() throws Exception
-    {
+    public String detectViolationFIO02J() throws Exception {
         ArrayList<Integer> lines = new ArrayList<Integer>();
         ArrayList<Integer> columns = new ArrayList<Integer>();
         ArrayList<Integer> ends = new ArrayList<Integer>();
         String fileDeleteInstance;
         boolean violation = false;
         MethodLevelCodeFragment cc1 = new MethodLevelCodeFragment();
-        for(int i=0;i<cc1.calledMethodNames.size();i++){
-            for(int j=0;j<cc1.ObjectCReationExpress.size();j++)
-            {
-                if((cc1.calledMethodNames.get(i).getNameAsString().equals("delete"))&&(cc1.ObjectCReationExpress.get(j).getTypeAsString().equals("File"))&&(cc1.calledMethodNames.get(i).getChildNodes().get(0).toString().equals(cc1.ObjectCReationExpress.get(j).getParentNode().get().getChildNodes().get(1).toString()))){
+        for (int i = 0; i < cc1.calledMethodNames.size(); i++) {
+            for (int j = 0; j < cc1.ObjectCReationExpress.size(); j++) {
+                if ((cc1.calledMethodNames.get(i).getNameAsString().equals("delete")) && (cc1.ObjectCReationExpress.get(j).getTypeAsString().equals("File")) && (cc1.calledMethodNames.get(i).getChildNodes().get(0).toString().equals(cc1.ObjectCReationExpress.get(j).getParentNode().get().getChildNodes().get(1).toString()))) {
                     fileDeleteInstance = cc1.calledMethodNames.get(i).getParentNode().get().toString();
-                    if(!cc1.IfStatements.isEmpty())
-                    {
-                        for(int k=0;k<cc1.IfStatements.size();k++)
-                        {
-                            if((!cc1.IfStatements.get(k).getCondition().toString().equals(fileDeleteInstance)))
-                            {
-                                if(!lines.contains(cc1.calledMethodNames.get(i).getBegin().get().line)){
+                    if (!cc1.IfStatements.isEmpty()) {
+                        for (int k = 0; k < cc1.IfStatements.size(); k++) {
+                            if ((!cc1.IfStatements.get(k).getCondition().toString().equals(fileDeleteInstance))) {
+                                if (!lines.contains(cc1.calledMethodNames.get(i).getBegin().get().line)) {
                                     lines.add(cc1.calledMethodNames.get(i).getBegin().get().line);
                                     columns.add(cc1.calledMethodNames.get(i).getBegin().get().column);
                                     ends.add(cc1.calledMethodNames.get(i).getEnd().get().column);
@@ -240,9 +221,8 @@ public class PackageLevelViolationDetector extends AnAction implements  Violatio
                                 violation = true;
                             }
                         }
-                    }
-                    else{
-                        if(!lines.contains(cc1.calledMethodNames.get(i).getBegin().get().line)){
+                    } else {
+                        if (!lines.contains(cc1.calledMethodNames.get(i).getBegin().get().line)) {
                             lines.add(cc1.calledMethodNames.get(i).getBegin().get().line);
                             columns.add(cc1.calledMethodNames.get(i).getBegin().get().column);
                             ends.add(cc1.calledMethodNames.get(i).getEnd().get().column);
@@ -252,13 +232,13 @@ public class PackageLevelViolationDetector extends AnAction implements  Violatio
                 }
             }
         }
-        if(violation){
+        if (violation) {
             lce.put("package_rule5_line", lines);
             lce.put("package_rule5_column", columns);
             lce.put("package_rule5_end", ends);
             valueFIO02J = "FIO02J violated at " + lines;
             return valueFIO02J;
         }
-        return  valueFIO02J;
+        return valueFIO02J;
     }
 }
