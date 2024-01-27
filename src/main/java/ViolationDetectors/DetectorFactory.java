c1782f26537f8ce017c324b465ff0850a3c7122d
Thilanka Bowala <thilankabowala@gmail.com>
2024-01-27 13:00:27 +0530

    Code refactor

--- src/main/java/ViolationDetectors/DetectorFactory.java
+++ src/main/java/ViolationDetectors/DetectorFactory.java
@@ -2,17 +2,17 @@ package ViolationDetectors;
 
 public class DetectorFactory {
 
-    public ViolationDetector getViolatorType(String ViolatorType){
-        if(ViolatorType == null){
+    public ViolationDetector getViolatorType(String ViolatorType) {
+        if (ViolatorType == null) {
             return null;
         }
-        if(ViolatorType.equalsIgnoreCase("MethodLevelViolationDetector")){
+        if (ViolatorType.equalsIgnoreCase("MethodLevelViolationDetector")) {
             return new MethodLevelViolationDetector();
 
-        } else if(ViolatorType.equalsIgnoreCase("ClassLevelViolationDetector")){
+        } else if (ViolatorType.equalsIgnoreCase("ClassLevelViolationDetector")) {
             return new ClassLevelViolationDetector();
 
-        } else if(ViolatorType.equalsIgnoreCase("PackageLevelViolationDetector")){
+        } else if (ViolatorType.equalsIgnoreCase("PackageLevelViolationDetector")) {
             return new PackageLevelViolationDetector();
         }
         return null;
