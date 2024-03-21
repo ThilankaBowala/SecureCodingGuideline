90ade88ba5e4668071a2147cdd39637d7e5a7e45
Thilanka Bowala <thilankabowala@gmail.com>
2024-03-21 08:28:41 +0530

    Validate AIML folder on plugin startup and edit

--- src/main/java/Tools/LiveParser.java
+++ src/main/java/Tools/LiveParser.java
@@ -1,15 +1,19 @@
 package Tools;
 
+import Configurations.Constants;
 import CustomViolationsDetectors.CustomViolationsDetectorSystem;
+import Startup.PluginStartupActivity;
 import com.intellij.codeInsight.hint.TooltipController;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.actionSystem.EditorActionManager;
 import com.intellij.openapi.editor.actionSystem.TypedAction;
 import com.intellij.openapi.editor.actionSystem.TypedActionHandler;
+import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowManager;
@@ -60,6 +64,8 @@ public class LiveParser extends AnAction {
     public static class MyTypedHandler implements TypedActionHandler {
         private final TypedActionHandler handler;
 
+        private static final Logger LOG = Logger.getInstance(MyTypedHandler.class);
+
         public MyTypedHandler(TypedActionHandler handler) {
             this.handler = handler;
         }
@@ -71,6 +77,20 @@ public class LiveParser extends AnAction {
             Project project = editor.getProject();
             Document document = editor.getDocument();
             editorText = document.getText();
+
+            if(!Constants.isReadyToWakeUp()){
+                try {
+                    PluginStartupActivity.setRuleStoreDirPathAtEdit(project);
+                } catch (ConfigurationException e) {
+                    LOG.warn("Warning: " + e);
+                }finally {
+                    if(!Constants.isReadyToWakeUp()){
+                        LOG.warn("Warning: Secure coding guidelines: Did not find your secure coding rules. If you want to detect violations of your secure coding guideline, please create your secure coding guideline rules first.");
+                        LOG.warn("Secure coding guidelines: If you need an example for this, please refer this project: 'https://github.com/ThilankaBowala/HelloWorld' and the custom secure coding rule store of it: 'https://github.com/ThilankaBowala/HelloWorld/tree/master/.idea/SecureCodingGuidelines'");
+                        return;
+                    }
+                }
+            }
 
             TooltipController tooltipController = new TooltipController();
             editor.addEditorMouseMotionListener(new MyEditorMouseMotionListener(tooltipController));
